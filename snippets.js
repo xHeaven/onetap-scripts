cc0c7b21035e44523169f02793eb958d42e5148c
xHeaven <magyarheaven@gmail.com>
2020-06-25 12:11:24 +0200

    added functions, cleanup
    Added 2 functions (IsVisible, IsHittable), and made some cleanup

--- snippets.js
+++ snippets.js
@@ -1,7 +1,7 @@
 /**
  * 
- * @param {entity} entity // The entity we want to check if the next shot on thorax/pelvis/body to it can be lethal
- * @returns {boolean} // Returns true if the entity can be killed with the above hitboxes with one bullet
+ * @param {entity} entity // The entity we want to check if the next shot on thorax/pelvis/body to it can be lethal.
+ * @returns {boolean} // Returns true if the entity can be killed with the above hitboxes with one bullet.
  */
 function IsLethal(entity) {
 	var entityHealth = Entity.GetProp(entity, "CBasePlayer", "m_iHealth");
@@ -25,8 +25,8 @@ function IsLethal(entity) {
 
 /**
  * 
- * @param {entity} entity // The entity we want to validate
- * @returns {boolean} // If the entity is valid, returns true, otherwise false
+ * @param {entity} entity // The entity we want to validate.
+ * @returns {boolean} // If the entity is valid, returns true, otherwise false.
  */
 function CheckEntity(entity) {
 	if (!Entity.IsValid(entity)) return false;
@@ -39,7 +39,7 @@ function CheckEntity(entity) {
 
 /**
  * 
- * @param {entity} entity // The entity whom KDR we want to get
+ * @param {entity} entity // The entity whom KDR we want to get.
  * @returns {float} // Returns the KDR of the given entity in the following format: 1.00
  */
 function GetKDR(entity) {
@@ -55,46 +55,132 @@ function GetKDR(entity) {
 
 /**
  * 
- * @param {entity} entity // The entity we want to check if it is in the air
- * @returns {boolean} // Returns true if the given entity is in the air, otherwise false
+ * @param {entity} entity // The entity we want to check if it is in the air.
+ * @returns {boolean} // Returns true if the given entity is in the air, otherwise false.
  */
 function IsInAir(entity) {
 	var flags = Entity.GetProp(entity, "CBasePlayer", "m_fFlags");
+
 	if (!(flags & 1 << 0) && !(flags & 1 << 0x12)) return true;
 	else return false;
 }
 
 /**
  * 
- * @param {entity} entity // The entity we want to check if it is crouching
- * @returns {boolean} // Returns true if the given entity is crouching, otherwise false
+ * @param {entity} entity // The entity we want to check if it is crouching.
+ * @returns {boolean} // Returns true if the given entity is crouching, otherwise false.
  */
 function IsCrouching(entity) {
 	var flags = Entity.GetProp(entity, "CBasePlayer", "m_fFlags");
+
 	if (flags & 1 << 1) return true;
 	else return false;
 }
 
 /**
  * 
- * @param {entity} entity // The entity we want to check if it is standing
- * @returns {boolean} // Returns true if the given entity is standing, otherwise false
+ * @param {entity} entity // The entity we want to check if it is standing.
+ * @returns {boolean} // Returns true if the given entity is standing, otherwise false.
  */
 function IsStanding(entity) {
 	var entityVelocity = Entity.GetProp(entity, "CBasePlayer", "m_vecVelocity[0]");
 	var entitySpeed = Math.sqrt(entityVelocity[0] * entityVelocity[0] + entityVelocity[1] * entityVelocity[1]);
+
 	if (entitySpeed <= 5) return true;
 	else return false;
 }
 
 /**
  * 
- * @param {entity} entity // The entity we want to check if it is slow walking
- * @returns {boolean} // Returns true if the given entity is slow walking, otherwise false
+ * @param {entity} entity // The entity we want to check if it is slow walking.
+ * @returns {boolean} // Returns true if the given entity is slow walking, otherwise false.
  */
 function IsSlowWalking(entity) {
 	var entityVelocity = Entity.GetProp(entity, "CBasePlayer", "m_vecVelocity[0]");
 	var entitySpeed = Math.sqrt(entityVelocity[0] * entityVelocity[0] + entityVelocity[1] * entityVelocity[1]);
+
 	if (entitySpeed >= 10 && entitySpeed <= 85) return true;
 	else return false;
+}
+
+/**
+ * 
+ * @param {entity} entity // The entity we want to check if visible.
+ * @param {integer/object} hitboxes // The hitboxes we want to check if visible on the given entity. It can be a single number, or an array of numbers.
+ * @returns {boolean} // Returns true if at least one of the given hitbox(es) is/are visible on the given entity.
+ */
+function IsVisible(entity, hitboxes) {
+	var localPlayer = Entity.GetLocalPlayer();
+	var localPlayerEyePos = Entity.GetEyePosition(localPlayer);
+
+	if (typeof (hitboxes) === "number") {
+		var hitboxPos = Entity.GetHitboxPosition(entity, hitboxes);
+		var trace = Trace.Line(localPlayer, localPlayerEyePos, hitboxPos);
+
+		if (trace[1] > 0.9)
+			return true;
+	}
+
+	if (typeof (hitboxes) === "object") {
+		hitboxes.forEach(function (hitbox) {
+			var hitboxPos = Entity.GetHitboxPosition(entity, hitbox);
+			var trace = Trace.Line(localPlayer, localPlayerEyePos, hitboxPos);
+
+			if (trace[1] > 0.9)
+				return true;
+		});
+	}
+
+	return false;
+}
+/**
+ * 
+ * @param {entity} entity // The entity we want to check if hittable.
+ * @param {integer/object} hitboxes // The hitboxes we want to check if hittable on the given entity. It can be a single number, or an array of numbers.
+ * @returns {object} // Returns an object that contains the following values: visible (bool), hittable (bool), damage (int), hitbox (int)
+ */
+function IsHittable(entity, hitboxes) {
+	var localPlayer = Entity.GetLocalPlayer();
+	var localPlayerEyePos = Entity.GetEyePosition(localPlayer);
+
+	var result = {
+		visible: false,
+		hittable: false,
+		damage: 0,
+		hitbox: 0
+	};
+
+	if (typeof (hitboxes) === "number") {
+		var hitboxPos = Entity.GetHitboxPosition(entity, hitboxes);
+		var trace = Trace.Bullet(localPlayer, localPlayerEyePos, hitboxPos);
+		var possibleDamage = trace[1];
+
+		if (possibleDamage > result.damage) {
+			result.visible = trace[2];
+			result.hittable = true;
+			result.damage = trace[1];
+			result.hitbox = hitboxes;
+
+			return result;
+		}
+	}
+
+	if (typeof (hitboxes) === "object") {
+		hitboxes.forEach(function (hitbox) {
+			var hitboxPos = Entity.GetHitboxPosition(entity, hitbox);
+			var trace = Trace.Bullet(localPlayer, localPlayerEyePos, hitboxPos);
+			var possibleDamage = trace[1];
+
+			if (possibleDamage > result.damage) {
+				result.visible = trace[2];
+				result.hittable = true;
+				result.damage = trace[1];
+				result.hitbox = hitbox;
+
+				return result;
+			}
+		});
+	}
+
+	return result;
 }
